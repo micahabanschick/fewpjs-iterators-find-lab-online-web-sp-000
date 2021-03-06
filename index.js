1dc671e73b2c1e98920f57b0e6a36b1c954ab10a
Micah Banschick <micha.ban@gmail.com>
2021-03-06 01:48:00 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(team) {
+  let found = team.find(team => team.result === "W");
+  if (!!found) {
+    return found.year;
+  } else {
+    return undefined;
+  }
+}
